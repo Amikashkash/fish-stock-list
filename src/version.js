778758755b462b7dcfa2b085aee545d20f0dabf2
Amikashkash <40613008+Amikashkash@users.noreply.github.com>
2025-12-09 20:50:14 +0200

    chore: Bump version to 1.2.0 - Fish Reception System

--- src/version.js
+++ src/version.js
@@ -3,6 +3,6 @@
  * Update this file before each release
  */
 
-export const VERSION = '1.1.2'
-export const RELEASE_DATE = '2025-12-07'
-export const RELEASE_NAME = 'Bugfix Release'
+export const VERSION = '1.2.0'
+export const RELEASE_DATE = '2025-12-09'
+export const RELEASE_NAME = 'Feature Release'
