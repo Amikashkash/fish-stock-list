a0fc03dc2573257322449a3cf973402af6165ba0
Amikashkash <40613008+Amikashkash@users.noreply.github.com>
2025-12-11 20:23:54 +0200

    chore: Bump version to 1.3.2

--- src/version.js
+++ src/version.js
@@ -3,6 +3,6 @@
  * Update this file before each release
  */
 
-export const VERSION = '1.3.1'
+export const VERSION = '1.3.2'
 export const RELEASE_DATE = '2025-12-11'
-export const RELEASE_NAME = 'Reception Workflow Redesign - Bug Fixes'
+export const RELEASE_NAME = 'Reception Workflow Redesign - Edit Plans UI'
