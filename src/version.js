eb2678272288bf29fd7df5028f093e20058615ca
Amikashkash <acfish.il@gmail.com>
2025-12-22 12:27:54 +0200

    chore: Bump version to 1.7.0
    - מערכת תכנון וביצוע העברות מלאה
    - 4 קומפוננטות חדשות
    - 2 כפתורים חדשים בדף הבית
    - תמיכה במצב וירטואלי ובדיקת התנגשויות

--- src/version.js
+++ src/version.js
@@ -3,6 +3,6 @@
  * Update this file before each release
  */
 
-export const VERSION = '1.6.2'
-export const RELEASE_DATE = '2025-12-21'
-export const RELEASE_NAME = 'Bugfix Release'
+export const VERSION = '1.7.0'
+export const RELEASE_DATE = '2025-12-22'
+export const RELEASE_NAME = 'Feature Release'
