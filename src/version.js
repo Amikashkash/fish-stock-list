a6b7eadc8c3fe285debabc5ccb5555b985f69c77
Amikashkash <acfish.il@gmail.com>
2026-01-14 16:13:30 +0200

    fix: עדכון מספר גרסה ל-19.5.5 בקובץ version.js

--- src/version.js
+++ src/version.js
@@ -3,6 +3,6 @@
  * Update this file before each release
  */
 
-export const VERSION = '19.5.3'
-export const RELEASE_DATE = '2026-01-13'
-export const RELEASE_NAME = 'Bugfix Release'
+export const VERSION = '19.5.5'
+export const RELEASE_DATE = '2026-01-14'
+export const RELEASE_NAME = 'UI Enhancement'
