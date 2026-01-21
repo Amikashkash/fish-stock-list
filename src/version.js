0eb8395c2b25db1266025b8cc9655cc8eaa9409b
Amikashkash <acfish.il@gmail.com>
2026-01-21 13:12:17 +0200

    fix: עדכון גרסה ל-19.5.14

--- src/version.js
+++ src/version.js
@@ -3,6 +3,6 @@
  * Update this file before each release
  */
 
-export const VERSION = '19.5.13'
+export const VERSION = '19.5.14'
 export const RELEASE_DATE = '2026-01-21'
-export const RELEASE_NAME = 'Simplified Excel Parser'
+export const RELEASE_NAME = 'Excel Parser Fix'
